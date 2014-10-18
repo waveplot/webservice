05abd73d0e87a39776b6451c1fdf918dfc3057da
Ben Ockmore <ben.sput@gmail.com>
2014-10-19 00:51:27 +0100

    Added endpoint to list all artist credits.

--- wpws/artist_credit.py
+++ wpws/artist_credit.py
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from flask.ext.restful import Resource
-import wpschema
+from flask.ext.restful import Resource, reqparse
+from wpschema import ArtistCredit
 
 from wpws import db
 
@@ -25,12 +25,41 @@ class ArtistCreditResource(Resource):
 
     def get(self, _id):
 
-        res = db.session.query(wpschema.ArtistCredit).filter_by(id=_id).first()
+        res = db.session.query(ArtistCredit).filter_by(id=_id).first()
 
         res = {
             'id': res.id,
             'name': res.name,
-            'credit': str(res.created),
+            'created': str(res.created),
+        }
+
+        return res
+
+
+class ArtistCreditListResource(Resource):
+
+    get_parser = reqparse.RequestParser()
+    get_parser.add_argument('limit', type=int, default=20)
+    get_parser.add_argument('offset', type=int, default=0)
+
+    def get(self):
+
+        args = self.get_parser.parse_args()
+
+        results = db.session.query(ArtistCredit).order_by('name')
+        results = results.offset(args.offset).limit(args.limit).all()
+
+        res = {
+            'start': args.offset,
+            'count': len(results),
+            'objects': [
+                {
+                    'id': credit.id,
+                    'name': credit.name,
+                    'created': str(credit.created),
+                }
+                for credit in results
+            ],
         }
 
         return res
@@ -38,3 +67,4 @@ class ArtistCreditResource(Resource):
 
 def create_api(api):
     api.add_resource(ArtistCreditResource, '/api/artist_credit/<int:_id>')
+    api.add_resource(ArtistCreditListResource, '/api/artist_credit')
